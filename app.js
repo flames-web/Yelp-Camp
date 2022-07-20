0e4927c4f7721b8f02c471f7078a8aba93b05b88
flames-web <rajiolalekanh247@gmail.com>
2022-07-20 01:33:06 +0100

    updated app.js

--- app.js
+++ app.js
@@ -175,6 +175,9 @@ app.use((err,req,res,next) => {
     res.status(status).render('error',{err});
 })
 
-app.listen(3000, () => {
-    console.log('Listening at port 3000');
+const port = process.env.PORT || 3000;
+
+
+app.listen(port, () => {
+    console.log(`Listening at port ${port}`);
 })
