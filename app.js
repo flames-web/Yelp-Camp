0dc7400e6ded3fc98c8f50fc804ed7c4cd9fd6e7
flames-web <rajiolalekanh247@gmail.com>
2022-07-25 10:59:01 +0100

    made some validations

--- app.js
+++ app.js
@@ -25,7 +25,9 @@ const MongoStore  = require('connect-mongo');
 
   
   
-  const dbUrl = process.env.DB_URL;
+  const dbUrl =   process.env.DB_URL;
+  // 'mongodb://localhost:27017/yelp-camp1'
+
 
 
  
