2a5a801f3c0584067b988f7787e87b0f90676753
flames-web <rajiolalekanh247@gmail.com>
2022-08-27 08:16:46 +0100

    new

--- app.js
+++ app.js
@@ -26,8 +26,9 @@ const MongoStore  = require('connect-mongo');
 
 const app = express();  
   
-  const dbUrl =   'mongodb://localhost:27017/yelp-camp1'
-//   process.env.DB_URL;
+  const dbUrl =  process.env.DB_URL;
+  // 'mongodb://localhost:27017/yelp-camp1'
+
 
 mongoose.connect(dbUrl,{
     useNewUrlParser : true ,
